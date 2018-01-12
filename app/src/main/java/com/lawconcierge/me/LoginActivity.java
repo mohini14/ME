9253c7deb3596438cbe21d6cdc00382688de6c19
Mohini Sindhu <mohini.sindhu14@gmail.com>
2018-01-12 15:30:55 +0530

    login functionality done

--- app/src/main/java/com/lawconcierge/me/LoginActivity.java
+++ app/src/main/java/com/lawconcierge/me/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.lawconcierge.me;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,20 +17,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setUpLayoutAttributes();
-        manageKeyboardOnScreen();
+        setUpLayoutAttributes( );
     }
 
     @Override
     public void onClick(View view) {
 
-        int viewId = view.getId();
-
-        switch (viewId){
+        switch (view.getId( )) {
 
             case R.id.signinButton :
 
-                signUpButtonPressed();
+                signInButtonPressed( );
+                break;
+
+            case R.id.createAccountButton :
+
+                createNewAccountButtonPressed( );
+                break;
+
+            case R.id.forgotPasswordButton :
+
+                forgotPasswordButtonPressed( );
                 break;
         }
     }
@@ -36,29 +45,45 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     /**
      * method is used to link layout views with class objects
      */
-    private void setUpLayoutAttributes(){
+    private void setUpLayoutAttributes() {
 
         mUsernameTextField = findViewById(R.id.usernameEditText);
         mPasswordTextField = findViewById(R.id.passwordEditText);
+
         findViewById(R.id.signinButton).setOnClickListener(this);
+        findViewById(R.id.createAccountButton).setOnClickListener(this);
+        findViewById(R.id.forgotPasswordButton).setOnClickListener(this);
+
+        Utility.manageKeyboardOnView(this, R.id.loginlayout1);
     }
 
-    // method manages the keyboard on touch
-    public void manageKeyboardOnScreen(){
 
-        Utility.hideKeyboard(this);
-    }
-
-    // Actions on activity
-
-    private void signUpButtonPressed() {
+    /**
+     * Method on success login credential opens home screen else shows error message.
+     */
+    private void signInButtonPressed() {
 
         String username = this.mUsernameTextField.getText( ).toString( );
         String password = this.mPasswordTextField.getText( ).toString( );
 
-        if(Validate.validateEmail(this, username)  && Validate.validatePassword(this, password)){
+        if (Validate.validateEmail(this, username) && Validate.validatePassword(this, password)) {
             //todo
         }
+    }
 
+    /**
+     * Method opens create new account page
+     */
+    private void createNewAccountButtonPressed() {
+        Intent myIntent = new Intent(this, CreateAccountActivity.class);
+        this.startActivity(myIntent);
+    }
+
+    /**
+     * Method opens Forgot password screen
+     */
+    private void forgotPasswordButtonPressed() {
+        Intent myIntent = new Intent(this, ForgotPasswordActivity.class);
+        this.startActivity(myIntent);
     }
 }
--- app/src/main/java/com/lawconcierge/me/Utility.java
+++ app/src/main/java/com/lawconcierge/me/Utility.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
@@ -21,6 +22,18 @@ public class Utility {
             ((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE)).
                     hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
+    }
+
+    public  static void manageKeyboardOnView(final Activity activity, int id){
+
+        activity.findViewById(id).setOnTouchListener(new View.OnTouchListener( ) {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Utility.hideKeyboard(activity);
+                return false;
+            }
+        });
     }
 
     public static void showAlertBox(Activity activity, String message){
